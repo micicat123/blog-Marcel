d13eb17fb8c0649acb9c5dd98126e6b41c473997
micicat123 <mici2002>
2021-02-09 16:29:54 +0100

    Dodal sem news

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_135915) do
+ActiveRecord::Schema.define(version: 2021_02_09_145927) do
+
+  create_table "news", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posta", force: :cascade do |t|
     t.string "title"
