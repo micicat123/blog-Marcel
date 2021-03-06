6d6e51ea7084c2365e52d6e15ae80040ab02b83b
micicat123 <mici2002>
2021-03-06 20:30:15 +0100

    Dodajanje bootstrapa in urejanje map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,16 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("bootstrap")
+
+// CSS
+import 'scss/site'
+// JS
+import('js/site')
+// Images
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
