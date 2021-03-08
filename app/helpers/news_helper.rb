adee54934bc56ac491191bf736f79241369f5f16
micicat123 <mici2002>
2021-03-08 12:20:07 +0100

    Konec projekta

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,21 +1,22 @@
 module NewsHelper
 
+
+
   def display_news_link(news)
-    html =  "<td>#{link_to 'Show', news}</td>"
-    if current_user == news.user
-      html += "<td>#{link_to 'Edit', edit_news_path(news)}</td>"
-      html += "<td>#{link_to 'Destroy', news, method: :delete, data: { confirm: 'Are you sure?' }}</td>"
+    capture do
+      concat link_to('Preberi več', news, class: 'stretched-link')
     end
-    html.html_safe
   end
 
   def display_news_show_links(news)
     html = ""
     if current_user == news.user
-      html += link_to 'Edit', edit_news_path(news)
+      html += link_to 'Uredi', edit_news_path(news)
+      html += "     "
+      html += link_to('Izbriši', news, method: :delete, data: { confirm: 'Ali ste prepričani?'})
       html += ' | '
     end
-    html += link_to 'Back', news_index_path
+    html += link_to 'Nazaj', news_index_path
     html.html_safe
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,21 +1,20 @@
 module PostsHelper
 
   def display_posts_link(post)
-    html =  "<td>#{link_to 'Show', post}</td>"
-    if current_user == post.user
-      html += "<td>#{link_to 'Edit', edit_post_path(post)}</td>"
-      html += "<td>#{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }}</td>"
+    capture do
+      concat link_to('Preberi več', post, class: 'stretched-link')
     end
-    html.html_safe
   end
 
   def display_post_show_links(post)
     html = ""
     if current_user == post.user
-      html += link_to 'Edit', edit_post_path(post)
+      html += link_to 'Uredi', edit_post_path(post)
+      html += "     "
+      html += link_to('Izbriši', post, method: :delete, data: { confirm: 'Ali ste prepričani?'})
       html += ' | '
     end
-    html += link_to 'Back', posts_path
+    html += link_to 'Nazaj', posts_path
     html.html_safe
   end
 end
